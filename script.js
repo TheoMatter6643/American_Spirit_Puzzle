f7b9b289bded6ce4e605930988d686ad7284b233
Theo Matter <theomatter2@gmail.com>
2026-05-18 10:42:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -90,8 +90,9 @@ initDragAndDrop();
 const board = document.getElementById("board");
 const piecesContainer = document.getElementById("pieces");
 
-const pieceWidth = 435;
-const pieceHeight = 438;
+const pieceWidth = 150;
+const pieceHeight = 150;
+
 
 let draggedPiece = null;
 
