1ce568849b15b4d3b1339cc1be189b8f80817314
Theo Matter <theomatter2@gmail.com>
2026-06-12 21:18:54 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
 const CACHE_NAME = "puzzle-cache-v1";
-
 const FILES_TO_CACHE = [
   "index.html",
   "style.css",
@@ -36,7 +35,7 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(() => new Response('', { status: 408 }));
     })
   );
 });
